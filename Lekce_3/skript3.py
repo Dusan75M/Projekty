62158287ae79a3e647efed28b553277b837dddaa
Dusan75M <machudusan@gmail.com>
2025-04-06 17:18:53 +0200

    Přidán print na řádku 12

---
+++ Lekce_3/skript3.py
@@ -0,0 +1,12 @@
+jmeno = "Marek"
+heslo = "1234"
+uzivatel = {"Marek": "1234"}
+
+if jmeno in uzivatel and heslo == "1234":
+    zprava = "Ahoj " + jmeno + ", vítej v aplikaci! Pokračuj..."
+else:
+    zprava = "Uživatelské jméno nebo heslo nejsou v pořádku!"
+
+print("Výstup:", zprava)
+
+print(Změna byla provedena)
